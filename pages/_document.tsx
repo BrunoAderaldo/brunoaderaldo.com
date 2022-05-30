3a86e557e5beb9144580274f19a6f55d62c02e71
Bruno Aderaldo <brunoaderaldo10@gmail.com>
2022-05-29 23:38:05 -0300

    feat: change theme

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,6 +12,43 @@ export default class Document extends NextDocument {
             id="stitches"
             dangerouslySetInnerHTML={{ __html: getCssText() }}
           />
+          {/* Preload fonts? */}
+
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/static/favicons/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/static/favicons/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/static/favicons/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/static/favicons/site.webmanifest" />
+          <link
+            rel="mask-icon"
+            href="/static/favicons/safari-pinned-tab.svg"
+            color="#5bbad5"
+          />
+          <meta name="msapplication-TileColor" content="#da532c" />
+          <meta name="theme-color" content="#090011" />
+
+          <link href="/static/favicons/favicon.ico" rel="shortcut icon" />
+          <meta
+            content="/static/favicons/browserconfig.xml"
+            name="msapplication-config"
+          />
+          <meta
+            content="max-snippet:-1, max-image-preview:large, max-video-preview:-1"
+            name="robots"
+          />
         </Head>
         <body>
           <Main />
--- stitches.config.ts
+++ stitches.config.ts
@@ -12,10 +12,11 @@ export const {
 } = createStitches({
   theme: {
     colors: {
-      background: '#F8FBFE',
-      primary: '#370665',
-      secondary: '#33286D',
-      accent: '#6C5CE7',
+      background: '#090011',
+      primary: '#F8FBFE',
+      secondary: '#AEB7C1',
+      // accent: '#6C5CE7',
+      accent: '#ca79e3',
     },
     fonts: {
       general: 'Poppins, sans-serif',
@@ -36,19 +37,17 @@ export const {
     bp3: '(min-width: 1024px)',
     bp4: '(min-width: 1280px)',
   },
-  // utils: {
-  //   marginX: value => ({ marginLeft: value, marginRight: value }),
-  // },
 })
 
-// export const darkTheme = createTheme({
-//   colors: {
-//     background: '#090011',
-//     primary: '#F8FBFE',
-//     secondary: '#AEB7C1',
-//     accent: '#6C5CE7',
-//   },
-// })
+export const lightTheme = createTheme({
+  colors: {
+    background: '#F8FBFE',
+    primary: '#370665',
+    secondary: '#33286D',
+    // accent: '#6C5CE7',
+    accent: '#ca79e3',
+  },
+})
 
 const globalStyles = globalCss({
   '*': {
@@ -64,7 +63,7 @@ const globalStyles = globalCss({
   h1: {
     color: '$primary',
     fontFamily: '$general',
-    fontSize: '42px',
+    fontSize: '32px',
     fontWeight: '$bold',
     lineHeight: '46px',
     margin: '0 0 20px',
@@ -72,47 +71,40 @@ const globalStyles = globalCss({
   h2: {
     color: '$primary',
     margin: '60px 0 0',
-    fontSize: '24px',
+    fontSize: '18px',
+    fontWeight: '$medium',
+    letterSpacing: '-.5px',
   },
   p: {
     color: '$secondary',
-    margin: '20px 0',
+    margin: '16px 0',
   },
   strong: {
     color: '$accent',
     fontWeight: '$semiBold',
   },
-  // a: {
-  //   borderBottom: '1px solid $primary',
-  //   color: '$primary',
-  //   textDecoration: 'none',
-  //   transition: 'opacity $duration ease-in-out',
-  // },
-  // 'a:hover, a:focus': {
-  //   opacity: '0.8',
-  // },
   '@font-face': [
     {
       fontFamily: 'Poppins',
-      src: `url("/static/font/Poppins-Regular.ttf") format("truetype")`,
+      src: `url("/fonts/Poppins-Regular.ttf") format("truetype")`,
       fontWeight: '400',
       fontStyle: 'normal',
     },
     {
       fontFamily: 'Poppins',
-      src: `url("/static/font/Poppins-Medium.ttf") format("truetype")`,
+      src: `url("/fonts/Poppins-Medium.ttf") format("truetype")`,
       fontWeight: '500',
       fontStyle: 'normal',
     },
     {
       fontFamily: 'Poppins',
-      src: `url("/static/font/Poppins-SemiBold.ttf") format("truetype")`,
+      src: `url("/fonts/Poppins-SemiBold.ttf") format("truetype")`,
       fontWeight: '600',
       fontStyle: 'normal',
     },
     {
       fontFamily: 'Poppins',
-      src: `url("/static/font/Poppins-Bold.ttf") format("truetype")`,
+      src: `url("/fonts/Poppins-Bold.ttf") format("truetype")`,
       fontWeight: '700',
       fontStyle: 'normal',
     },
