6917eacab812a359e9a5d2558237dc51e2816403
Bruno Aderaldo <brunoaderaldo10@gmail.com>
2022-10-31 22:07:21 -0300

    chore: add autoprefixer and cssnano

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,4 +6,13 @@ import sitemap from "@astrojs/sitemap";
 export default defineConfig({
   site: "https://brunoaderaldo.com",
   integrations: [sitemap()],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@import "./src/base/styles/_base.scss";`,
+        },
+      },
+    },
+  },
 });
